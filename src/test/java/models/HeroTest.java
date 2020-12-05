515975b7d928aa6d5112492182b6cdc36e55d4e8
keza1brenda <keza1brenda@gmail.com>
2020-12-05 03:37:44 +0200

    add first Hero test

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -11,11 +11,11 @@ public class HeroTest {
         Hero hero = new Hero("Raven",20,'F',"illusions","family issues");
         assertEquals(true, hero instanceof Hero);
     }
-
+/*
     @Test
     public void HeroInstantiatesWithName_true() throws Exception {
         Hero hero = new Hero("Batman",30,'M',"utility belt","no super power");
         assertEquals("Batman", hero.getName());
-    }
+    }*/
 
 }
